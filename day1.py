e0797c9599162f08363fee7206d559e31ba8d089
Imagine961 <williamdebourbon@gmail.com>
2025-12-01 19:21:51 +1100

    p1 and p2
    brute force method

---
+++ day1.py
@@ -0,0 +1,32 @@
+input_file = open("input.txt", "r")
+Input = input_file.read().splitlines()
+
+
+### BRUTE FORCE SOLUTION ###
+test = 0
+dial = 50
+code = 0
+
+for line in Input:
+    if line[0] == "R":
+        test = int(line[1:])
+        for i in range(test):
+            dial += 1
+            if dial == 100:
+                code += 1
+            if dial > 99:
+                dial = 0
+    elif line[0] == "L":
+        test = int(line[1:])
+        for i in range(test):
+            dial -= 1
+            if dial == 0:
+                code += 1
+            if dial < 0:
+                dial = 99
+
+
+print(code)
+
+
+### OPTIMIZED SOLUTION ###
